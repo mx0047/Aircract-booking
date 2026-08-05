f3fb824b84a46e5fb4391161439a9619ebddea6a
mx0047 <117757459+mx0047@users.noreply.github.com>
2026-08-05 11:06:08 +0200

    Upload to root: js/app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-import DataStore from './data.js';
-import VFR from './vfr.js';
-import Auth from './auth.js';
-import Booking from './booking.js';
-import Calendar from './calendar.js';
-import Admin from './admin.js';
+import DataStore from './data.js?v=1.0.1';
+import VFR from './vfr.js?v=1.0.1';
+import Auth from './auth.js?v=1.0.1';
+import Booking from './booking.js?v=1.0.1';
+import Calendar from './calendar.js?v=1.0.1';
+import Admin from './admin.js?v=1.0.1';
 
 const App = {
     currentScreen: null,
